38ad32df40a1c0b6630316d068801877be2e0218
Nhat Anh <nhatanhkof@gmail.com>
2024-12-27 19:43:14 +0700

    update config2

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
+// doi config vite
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
